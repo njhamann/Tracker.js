9b5952bbdf5d6d88cc645f7b25a37c8a90e869d9
Noah Hamann <njhamann@gmail.com>
2014-01-21 20:16:01 +0000

    added grunt builder

--- src/ga-event-tracking.js
+++ src/ga-event-tracking.js
@@ -1,37 +1,18 @@
 /**
- * Google Analytic Event Tracking
- * just add 'data-ga-click="compose button"'
- * to any element to start tracking events
+ * Tracker.js
+ * description:  Google Analytics event tracking
+ * version:      0.1.0
+ * authors:      Noah Hamann
+ * repo:         https://github.com/njhamann/Tracker.js
+ *
  * to do: 
- * - create live events
  * - create a white list function for
  *   free-for-all event tracking
  */
 
 (function(){
-    var gaEvents = (function(){
-
-        /*
-        <a href="#" 
-            data-ga-track 
-            data-ga-category="publish" 
-            data-ga-action="click" 
-            data-ga-label="compose" 
-            data-ga-value="22" 
-            data-ga-non-interaction="true">Anchor tag</a>
-        
-        <a href="#" 
-            data-ga-click="compose button" 
-            data-ga-category="publish" 
-            data-ga-action="click" 
-            data-ga-label="compose button overwrite label" 
-            data-ga-value="22" 
-            data-ga-non-interaction="true">Anchor tag</a>
-
-        <a href="#" data-ga-click="compose button">Anchor tag</a>
-        <a href="#" data-ga-focus="compose main textarea">Anchor tag</a>
-        */
-
+    var GAEventTracking = (function(){
+        //private
         function addLiveListener(eventType, selector, callback){
             if(!eventType || !selector) return;
             document.addEventListener(eventType, function (e) {
@@ -39,16 +20,12 @@
                     found;
                 
                 while (el) {
-                    if(found = Sizzle.matchesSelector(el, selector)) {
-                        console.log(el);
-                        console.log(selector);
-                        break;
-                    }
+                    found = Sizzle.matchesSelector(el, selector);
+                    if(found) break;
                     el = el.parentElement;
                 }
 
                 if (found && callback) {
-                    console.log(el);
                     callback.call(el, e);
                 }
             });
@@ -63,7 +40,6 @@
 
         function pushEvent(e){
             e.preventDefault();
-            console.log(e.target);
             var elm = this,
                 gaCategory = elm.getAttribute('data-ga-category') || 'app',
                 gaAction = elm.getAttribute('data-ga-action') || e.type || null,
@@ -80,6 +56,28 @@
         addListeners('[data-ga-click]', 'click');
         addListeners('[data-ga-focus]', 'focus');
         addListeners('[data-ga-blur]', 'blur');
+        
+        //public
+        var addTracking = function(tracking){
+            /**
+             * whitelisting
+             * GAEventTracking.addTracking(selector, options);
+             * GAEventTracking.addTracking('a, button', {
+             *     events:          'click, focus, blur',
+             *     category:        'cat',
+             *     action:          'click',
+             *     label:           'cool clikc',
+             *     value:           20,
+             *     non_interaction: true
+             * });
+             */
+            
+        };
+
+        return {
+            addTracking: addTracking
+        };
 
     })();
+    window.GAEventTracking = GAEventTracking;
 })();
