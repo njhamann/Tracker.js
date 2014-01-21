06072b73e34ffea0637c4a75761d14be122e6da0
Noah Hamann <njhamann@gmail.com>
2014-01-21 17:37:58 +0000

    added live events

--- src/ga-event-tracking.js
+++ src/ga-event-tracking.js
@@ -32,40 +32,42 @@
         <a href="#" data-ga-focus="compose main textarea">Anchor tag</a>
         */
 
-        function addLiveListeners(eventType, selector, callback){
-            document.addEventListener(eventType, function (event) {
-                var el = event.target,
+        function addLiveListener(eventType, selector, callback){
+            if(!eventType || !selector) return;
+            document.addEventListener(eventType, function (e) {
+                var el = e.target,
                     found;
-                console.log(el); 
+                
                 while (el) {
-                    console.log(selector);
-                    console.log(el);
-                    console.log(Sizzle.matchesSelector(el, selector));
-                    if(found = Sizzle.matchesSelector(el, selector)) break;
+                    if(found = Sizzle.matchesSelector(el, selector)) {
+                        console.log(el);
+                        console.log(selector);
+                        break;
+                    }
                     el = el.parentElement;
                 }
 
-                if (found) {
-                    callback.call(el, event);
+                if (found && callback) {
+                    console.log(el);
+                    callback.call(el, e);
                 }
             });
         } 
 
-        function addListeners(elements, eventType){
+        function addListeners(selector, eventType){
             var events = eventType.replace(/\s+/g, '').split(',');
-            for(var a = 0; a < elements.length; a++){
-                for(var b = 0; b < events.length; b++){
-                    elements[a].addEventListener(events[b], pushEvent);
-                }
+            for(var b = 0; b < events.length; b++){
+                addLiveListener(events[b], selector, pushEvent);
             }
         }
 
         function pushEvent(e){
             e.preventDefault();
-            var elm = e.currentTarget,
+            console.log(e.target);
+            var elm = this,
                 gaCategory = elm.getAttribute('data-ga-category') || 'app',
                 gaAction = elm.getAttribute('data-ga-action') || e.type || null,
-                gaLabel = elm.getAttribute('data-ga-label') || elm.getAttribute('data-ga-click') || $this.text(),
+                gaLabel = elm.getAttribute('data-ga-label') || elm.getAttribute('data-ga-click') || elm.innerHTML.replace(/<[^>]*>/g, ""),
                 gaValue = parseInt(elm.getAttribute('data-ga-value'), 10) || null,
                 gaNonInteraction = elm.getAttribute('data-ga-non-interaction') == 'true';
             
@@ -74,25 +76,10 @@
             }
         }
 
+        addListeners('[data-ga-track]', 'click,focus,blur');
+        addListeners('[data-ga-click]', 'click');
+        addListeners('[data-ga-focus]', 'focus');
+        addListeners('[data-ga-blur]', 'blur');
 
-        var trackElms = document.querySelectorAll('[data-ga-track]'),
-            clickElms = document.querySelectorAll('[data-ga-click]'),
-            focusElms = document.querySelectorAll('[data-ga-focus]'),
-            blurElms = document.querySelectorAll('[data-ga-blur]');
-
-        addListeners(trackElms, 'click,focus,blur');
-        addListeners(clickElms, 'click');
-        addListeners(focusElms, 'focus');
-        addListeners(blurElms, 'blur');
-
-        addLiveListeners('click', '#ttesting_live', function(e){
-            console.log(e);
-        });
-
-        //removing jquery dep
-        //$doc.on('click', '[data-ga-track], [data-ga-click]', pushEvent);
-        //$doc.on('focus', '[data-ga-track], [data-ga-focus]', pushEvent);
-        //$doc.on('blur', '[data-ga-track], [data-ga-blur]', pushEvent);
-        
     })();
 })();
